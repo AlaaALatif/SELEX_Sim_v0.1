c674957bb3b6f248c25f89d532f7072645b92f34
Al <al.a.latif.94@gmail.com>
2016-07-28 14:18:16 +0100

    appends and brute search

--- Mutation.py
+++ Mutation.py
@@ -14,24 +14,33 @@ class Mutation:
 # randomly mutates each seq copy in mutatedPool using the corresponding
 # random positions. random mutations can be circular (i.e. A to A)
 # mutated seqs are added to the amplfied pool of wild-type seqs
-    def mutGen(self, seqLength, mutatedPool, amplfdSeqs, alphabetSet):
+    def mutGen(self, seqLength, mutatedPool, amplfdSeqs, aptamerSeqs, alphabetSet):
         alphabetSize = len(alphabetSet)
+        amplfdSeqs = amplfdSeqs.tolist()
         for seqInfo in mutatedPool:
-            seq = apt.pseudoAptamerGenerator(seqInfo[0], alphabetSet, seqLength) #fetch sequence
+            mutatedSeq = apt.pseudoAptamerGenerator(seqInfo[0], alphabetSet, seqLength) #fetch sequence
             mutNum = mutatedPool[seqInfo][0] #no. of mutation instances
             i=1
             while(mutatedPool[seqInfo][0] > 0): #for each seq copy
                 mut = mutatedPool[seqInfo][i] # grab the i-th mutation instance for a given seq
                 for j in mut: #for each mutation position in the instance
                     randNuc = alphabetSet[random.randint(0, alphabetSize - 1)] #pick random nt
-                    mutatedSeq = seq[:(j-1)] + randNuc + seq[j:] #mutate seq using random nt
+                    mutatedSeq = mutatedSeq[:(j-1)] + randNuc + mutatedSeq[j:] #mutate seq using random nt
                 mutatedSeqIdx = apt.pseudoAptamerIndexGenerator(mutatedSeq, alphabetSet, seqLength)
                 if mutatedSeqIdx not in amplfdSeqs[:,0]: # add mutated seq to the amplified pool
-                    amplfdSeqs.setdefault(mutatedSeq, []).append(1)
+                    mutatedSeqInfo = np.zeros(4)
+                    mutatedSeqInfo[0] = mutatedSeqIdx
+                    mutatedSeqInfo[1] = 1
+                    mutatedSeqInfo[2] = d.hamming_func(mutatedSeq, aptamerSeqs)
+                    mutatedSeqInfo[3] = d.bias_func(mutatedSeq, seqLength)
+                    amplfdSeqs.append(mutatedSeqInfo)
                 else:
-                    amplfdSeqs[mutatedSeq][0] += 1
+                    for seqInfo in amplfdSeq:
+                        if(seqInfo[0] == mutatedSeqIdx):
+                            amplfdSeqs[amplfdSeqs.index(seqInfo)][1] += 1
+                            break
                 mutatedPool[seq][0] -= 1 #decrement no. of seq copies to be mutated
                 i += 1
-        outPool = amplfd_seqs
+        outPool = np.asarray(amplfdSeqs)
         print("Mutation has been carried out")
         return outPool
