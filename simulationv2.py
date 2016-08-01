6cc267c66d4f4f08aea113eab5cdcf67768f4685
A.A.A. Abdel-Latif <aaaa3@poros.lsc.phy.cam.ac.uk>
2016-08-01 22:14:51 +0100

    updated

--- simulationv2.py
+++ simulationv2.py
@@ -1,9 +1,9 @@
 import sys
 import Aptamers
-import Distance
 import Selection
 import Amplification
 import Mutation
+import utils
 
 aptamerType = str(sys.argv[1])
 aptamerNum = int(sys.argv[2])
@@ -16,20 +16,22 @@ pcrErrorRate = float(sys.argv[8])
 outputFileNames = str(sys.argv[9])
 
 # Instantiating classes
-a = Aptamers.Aptamers()
-d = Distance.Distance()
-s = Selection.Selection()
-amplify = Amplification.Amplification()
-mut = Mutation.Mutation()
+Apt = Aptamers.Aptamers()
+S = Selection.Selection()
+Amplify = Amplification.Amplification()
+Mut = Mutation.Mutation()
+util = utils.utils()
 
 # SELEX simulation based on random aptamer assignment, hamming-based definite selection, and
 # non-ideal stochastic amplfication with no bias. 
 for r in range(roundNum):
-    if(i==0):
-        if(aptamerType = 'DNA'):
-            aptamerSeqs, initialSeqNum = a.optimumAptamerGenerator(aptamerNum, 'ACGT', seqLength)
-        elif(aptamerType = 'RNA'):
-            aptamerSeqs, initialSeqNum = a.optimumAptamerGenerator(aptamerNum, 'ACGU', seqLength)
+    if(r==0):
+        if(aptamerType == 'DNA'):
+            alphabetSet = 'ACGT'
+            aptamerSeqs, initialSeqNum = Apt.optimumAptamerGenerator(aptamerNum, alphabetSet, seqLength)
+        elif(aptamerType == 'RNA'):
+            alphabetSet = 'ACGU'
+            aptamerSeqs, initialSeqNum = Apt.optimumAptamerGenerator(aptamerNum, alphabetSet, seqLength)
         else:
             print("Error: Simulation of %.s aptamers not supported" %aptamerType)
             break
@@ -38,33 +40,32 @@ for r in range(roundNum):
         print("optimum sequences have been chosen")
         print("SELEX Round 1 has started")
         print("total number of sequences in initial library = "+str(initialSeqNum))
-        slctdSeqs = s.stochasticHammingSelection(seqLength, aptamerSeqs, selectionThreshold, initialSeqNum, i)
+
+        slctdSeqs = S.stochasticHammingSelection(alphabetSet, seqLength, None, aptamerSeqs, selectionThreshold, initialSeqNum, r)
         print("selection carried out for R0")
-        amplfdSeqs, mutatedPool = amplify.randomPCR_errorProne_biased(slctdSeqs, seqLength, num_pcr_cycles, pcrYield, errorRate) #change var name returned by amplification
+        amplfdSeqs, mutatedPool = Amplify.randomPCR_errorProne_biased(slctdSeqs, seqLength, pcrCycleNum, pcrYield, pcrErrorRate) #change var name returned by amplification
         print("amplification carried out for R0")
-        outPool = mut.mutGen(seqLength, mutatedPool, amplfdSeqs, aptamerSeqs, alphabetSet)
+        outPool = Mut.mutGen(seqLength, mutatedPool, amplfdSeqs, aptamerSeqs, alphabetSet)
         print("Mutation carried out for R0")
-        outFile = initPool + "_R" + str(i+1)
+        outFile = outputFileNames + "_R" + str(i+1)
         nxtRnd = open(outFile, 'w')
         print("writing R1 seqs to file")
-        for seq in outPool:
-            nxtRnd.write(seq[0]+'\n') #write seqIdx for now
+        for seqIdx in outPool:
+            nxtRnd.write(str(seqIdx)+'\t'+str(outPool[seqIdx][0])+'\n') #write seqIdx and count for now
+        nxtRnd.close()
     else:
         print("SELEX Round "+str(i+1)+" has started")
-        junk, totalSeqNum = a.optimumSeqs(nseq, outFile)
+        totalSeqNum, uniqSeqNum = util.seqNum(outPool)
         print("totalseqs = "+str(totalSeqNum))
-        slctdSeqs = s.stochasticSelection(seqLength, outPool, selectionRate, totalSeqNum)
+        slctdSeqs = S.stochasticSelection(alphabetSet, seqLength, outPool, None, selectionThreshold, totalSeqNum, r)
         print("Selection carried for R"+str(i+1))
-        amplfdSeqs, mutatedPool = amplify.ampEffMutDefinite(slctdSeqs, seqLength, num_pcr_cycles, pcrYield, errorRate)
+        amplfdSeqs, mutatedPool = Amplify.ampEffMutDefinite(slctdSeqs, seqLength, pcrCycleNum, pcrYield, pcrErrorRate)
         print("Amplification carried for R"+str(i+1))
-        del(slctdSeqs)
-        outPool = mut.mutGen(mutatedPool, amplfdSeqs)   
+        outPool = Mut.mutGen(mutatedPool, amplfdSeqs)   
         print("Mutation carried for R"+str(i+1))
-        del(amplfdSeqs)
-        del(mutatedPool)
-        outFile = initPool + "_R" + str(i+1)
+        outFile = outputFileNames + "_R" + str(i+1)
         nxtRnd = open(outFile, 'w')
         print("writing R"+str(i+1)+" seqs to file")
-        for seq in outPool[seq][0]:
-            for j in range(outPool[seq][0]):
-                nxtRnd.write(seq+'\n')
+        for seqIdx in outPool:
+            nxtRnd.write(str(seqIdx)+'\t'+str(outPool[seqIdx][0])+'\n') #write idx and count for now
+        nxtRnd.close()
