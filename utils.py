0be1dd9a6f1d2080db0721cf5a1022ff1e002ca9
A.A.A. Abdel-Latif <aaaa3@poros.lsc.phy.cam.ac.uk>
2016-08-10 00:44:45 +0100

    added seqNum and conv2dist methods

--- utils.py
+++ utils.py
@@ -9,11 +9,20 @@ import numpy as np
 
 
 
-class utils:
 
-    def seqNum(self, seqPool):
-        for seqIdx in seqPool:
-            totalSeqNum += seqPool[seqIdx][0]
-            uniqSeqNum += 1
+def seqNum(self, seqPool):
+    for seqIdx in seqPool:
+        totalSeqNum += seqPool[seqIdx][0]
+        uniqSeqNum += 1
 
-        return totalSeqNum, uniqSeqNum
+    return totalSeqNum, uniqSeqNum
+
+def convert_to_distribution(x, y, distName):
+    xDist = stats.rv_discrete(name=distName, values=(x, y))
+    return xDist
+
+# Add method for computing the binomial coefficient
+
+# Add method for computing the L1 norm 
+
+# Add method to convert probability vectors to discrete distributions
