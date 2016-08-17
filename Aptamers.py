7d9827eaf653a6420ac37d715fcbd595263e870c
Alaa Abdel Latif <aaaa3@eros.ch.private.cam.ac.uk>
2016-08-17 17:37:24 +0100

    a version that should work altogether

--- Aptamers.py
+++ Aptamers.py
@@ -79,37 +79,6 @@ class Aptamers:
         initLibrary = product(alphabetSet, repeat=seqLen)
         return initLibrary
 
-
-# choose a random subset of sequences to be aptamers from the initial pool
-# input params are the number of aptamers to choose and the initial pool 
-# sequence file
-# returns total number of seqs in initial pool and set of aptamers 
-    def randomAptamerChooser(self, aptamerNum, seqLen, initLib):                                 
-        initialSeqNum = 4**(seqLen)
-        optimumAptamers = np.chararray(aptamerNum, itemsize=seqLen)
-        for aptNum in xrange(aptamerNum):
-            aptIdx = random.randint(0, initialSeqNum) #random seq index
-            aptSeqList = list(islice(initLib, aptIdx, aptIdx+1))[0]
-            for residue in aptSeqList:
-                aptSeq += residue
-            optimumAptamers[aptNum] = aptSeq
-        return optimumAptamers, initialSeqNum
-# Generate all possible sequences 
-    def aptamerGenerator(self, alphabetSet, seqLength, start, finish, outFile):
-        initialLibrary = product(alphabetSet, repeat=seqLength)
-        #poolFraction = list(islice(initialLibrary, start, finish))
-        with open(outFile, 'w') as o:
-            for i in xrange(start, finish, 1000000):
-                seqList = list(islice(initialLibrary, (start+i), (start+i+1000000)))
-                for seq in seqList:
-                    sequence = str()
-                    for charIdx in range(seqLength):
-                        sequence += seq[charIdx]
-                    o.write(sequence+'\n')
-        o.close()
-        print("first generation completed")
-        return initialLibrary
-
 ###NEED TO MODIFY TO ALLOW MULTIPLE OPTIMUM APTAMERS
 
     def optimumAptamerGenerator(self, aptamerNum, alphabetSet, seqLen):
@@ -133,28 +102,3 @@ class Aptamers:
         seq = seq[::-1] #reverse string
         return seq, initialSeqNum
 
-
-
-# choose a random subset of sequences to be aptamers from the initial pool
-# input params are the number of aptamers to choose and the initial pool 
-# sequence file
-# returns total number of seqs in initial pool and set of aptamers 
-    def optimumSeqs(self, num_optm_seqs, init_pool_file):                                 
-        with open(init_pool_file) as f: #open initial library
-            for totalseqs, l in enumerate(f): #calculate total num of seqs
-                pass
-            totalseqs+=1
-        f.close()
-        print(totalseqs)
-        optimseqs_idx={}
-        optimseqs={}
-        i=1
-        while(i <= num_optm_seqs):
-            optimseq_idx=random.randint(0,totalseqs) #random seq index
-            if optimseq_idx not in optimseqs_idx:
-                optimseqs_idx.setdefault(optimseq_idx, [])                          
-                optimseq = str(open(init_pool_file).readlines()[optimseq_idx])
-                optimseqs.setdefault(optimseq, [])
-            i+=1
-        print(optimseqs)
-        return optimseqs, totalseqs
--- Distance.py
+++ Distance.py
@@ -6,12 +6,6 @@ import operator
 from collections import OrderedDict
 from scipy import stats
 
-def hamming_func(str1, str2):
-   assert len(str1) == len(str2)
-   ne = operator.ne
-   return sum(imap(ne, str1, str2))
-
-
 class Distance:
 
     def hamming_func(self, str1, str2):
@@ -26,74 +20,4 @@ class Distance:
                 pyrNum += 1 #increment no. of pyrimidines
         biasScore = 0.1*(2*pyrNum - seqLen)/seqLen #compute bias
         return biasScore
-
-    def biasedHamming_initLib(self, seqLen, optimseqs, pool_file, scale, partition):
-        seqs=np.zeros((scale, 3))
-        seqIdx = 0
-        with open(pool_file) as p:
-            while True:
-                next_n_seqs = list(islice(p, partition))
-                if not next_n_seqs:
-                    break
-                for i, seq in enumerate(next_n_seqs):
-                    seqs[seqIdx][0] += 1 #increment count
-                    seqs[seqIdx][1] = hamming_func(optimseqs, seq[:-1])
-                    for nt in seq[:-1]:
-                        if(nt == 'C') or (nt == 'T'):
-                            seqs[seqIdx][2] += 1 #increment no. of pyrimidines
-                    seqs[seqIdx][2] = 0.1*(2*seqs[seqIdx][2] - seqLen)/seqLen #compute bias
-                    seqIdx += 1
-        p.close()
-
-        return seqs
-
-##TEST AREA
-
-#d = Distance()
-#seqs = d.biasedHamming_initLib(20, 'AAAAAAAAAAAAAAAAAAAA', 'random_initLib_1KM', 100000000, 1000000)
-
-
-# NOTE: DISTANCES ARE NOW APPENDED AS A 3RD VALUE, NOT 2ND DUE TO BIAS
-# This method computes the hamming distance of each sequence to the optimum set
-# and their PCR bias. It requires sequence length as 3rd argument
-    def seqsHamming_and_Bias(self, optimseqs, pool_file, seqLen):
-        seqs={}
-        seqsfile = open(pool_file)
-        seq=seqsfile.readline()
-        while(seq):
-            if seq not in seqs:
-                seqs.setdefault(seq, []).append(1) #append seq count
-                seqs.setdefault(seq, []).append(0) #append seq bias
-                for nt in seq:
-                    if(nt == 'C') or (nt == 'T'):
-                        seqs[seq][1]+=1 #increment no. of pyrimidines
-                seqs[seq][1] = 0.1*(2*seqs[seq][1] - seqLen)/seqLen #compute bias
-                for optimseq in optimseqs:
-                    hammdist = hamming_func(optimseq, seq) #compute distance
-                    seqs.setdefault(seq, []).append(hammdist) #append distance
-            else:
-                seqs[seq][0]+=1 #increment count
-            seq=seqsfile.readline()
-        seqsfile.close()
-        print("distance calculations and bias scoring completed")
-        return seqs
-
-    def seqsHamming(self, optimseqs, pool_file):
-        seqs=np.zeros((scale, 3))
-        seqsfile = open(pool_file)
-        seq=seqsfile.readline()
-
-        while(seq):
-            if seq not in seqs:
-                seqs.setdefault(seq, []).append(1)
-            
-                for optimseq in optimseqs:
-                    hammdist = hamming_func(optimseq, seq)
-                    seqs.setdefault(seq, []).append(hammdist)
-            else:
-                seqs[seq][0]+=1
-            seq=seqsfile.readline()
-        seqsfile.close()
-        print("distance calculations passed")
-        return seqs
 
