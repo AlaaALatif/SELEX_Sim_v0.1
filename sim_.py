df07a30bec4f9ab80a61bb8da6aa6fdc4f153a87
Tristan Cragnolini <tc427@cam.ac.uk>
2017-02-14 14:14:29 +0000

    Deduplicate code in sim_.py

--- sim_.py
+++ sim_.py
@@ -25,108 +25,69 @@ S = Selection()
 Amplify = Amplification()
 Mut = Mutation()
 
-if(distanceMeasure == "hamming"):
-# SELEX simulation based on random aptamer assignment, hamming-based definite selection, and
-# non-ideal stochastic amplfication with no bias. 
-    for r in range(roundNum):
-        if(r==0):
-            if(aptamerType == 'DNA'):
-                alphabetSet = 'ACGT'
-                aptamerSeqs, initialSeqNum = Apt.optimumAptamerGenerator(aptamerNum, alphabetSet, seqLength)
-            elif(aptamerType == 'RNA'):
-                alphabetSet = 'ACGU'
-                aptamerSeqs, initialSeqNum = Apt.optimumAptamerGenerator(aptamerNum, alphabetSet, seqLength)
-            else:
-                print("Error: Simulation of %.s aptamers not supported" %aptamerType)
-                break
-            print("optimum sequences have been chosen")
-            print("SELEX Round 1 has started")
-            print("total number of sequences in initial library = "+str(initialSeqNum))
-            slctdSeqs = S.stochasticHammingSelection_initial(alphabetSet, seqLength, aptamerSeqs, selectionThreshold, initialSeqNum, samplingSize, outputFileNames, r)
-            print("selection carried out for R1")
-            amplfdSeqs = Amplify.randomPCR_with_ErrorsAndBias(slctdSeqs, seqLength, pcrCycleNum, pcrYield, pcrErrorRate, aptamerSeqs, alphabetSet, distanceMeasure)
-            print("amplification carried out for R1")
-            outFile = outputFileNames + "_R" + str(r+1)
-            nxtRnd = open(outFile, 'w')
-            print("writing R1 seqs to file")
-            for seqIdx in amplfdSeqs:
-                seq = Apt.pseudoAptamerGenerator(seqIdx, alphabetSet, seqLength)
-                nxtRnd.write(str(seq)+'\t'+str(int(amplfdSeqs[seqIdx][0]))+'\t'+str(int(amplfdSeqs[seqIdx][1]))+'\t'+'\n') #write seqIdx, count, distance, and bias...for now
-            nxtRnd.close()
-        else:
-            del(slctdSeqs)
-            print("SELEX Round "+str(r+1)+" has started")
-            totalSeqNum, uniqSeqNum = utils.seqNumberCounter(amplfdSeqs)
-            print("total number of sequences in initial pool = "+str(totalSeqNum))
-            print("total number of unique sequences in initial pool = "+str(int(uniqSeqNum)))
-            slctdSeqs = S.stochasticHammingSelection(alphabetSet, seqLength, amplfdSeqs, selectionThreshold, uniqSeqNum, totalSeqNum, samplingSize, outputFileNames, r)
-            print("Selection carried for R"+str(r+1))
-            del(amplfdSeqs)
-            amplfdSeqs = Amplify.randomPCR_with_ErrorsAndBias(slctdSeqs, seqLength, pcrCycleNum, pcrYield, pcrErrorRate, aptamerSeqs, alphabetSet, distanceMeasure)
-            print("Amplification carried for R"+str(r+1))
-            outFile = outputFileNames + "_R" + str(r+1)
-            nxtRnd = open(outFile, 'w')
-            print("writing R"+str(r+1)+" seqs to file")
-            for seqIdx in amplfdSeqs:
-                seq = Apt.pseudoAptamerGenerator(seqIdx, alphabetSet, seqLength)
-                nxtRnd.write(str(seq)+'\t'+str(int(amplfdSeqs[seqIdx][0]))+'\t'+str(int(amplfdSeqs[seqIdx][1]))+'\n') #write idx and count for now
-            nxtRnd.close()
-    print("SELEX completed")
 
-elif(distanceMeasure == "basepair"):
+if distanceMeasure not in ("hamming", "basepair"):
+    print("Invalid argument for distance measure")
+    sys.exit()
+
 # SELEX simulation based on random aptamer assignment, hamming-based definite selection, and
 # non-ideal stochastic amplfication with no bias. 
-    for r in range(roundNum):
-        if(r==0):
-            if(aptamerType == 'DNA'):
-                alphabetSet = 'ACGT'
-                aptamerSeqs, initialSeqNum = Apt.optimumAptamerGenerator(aptamerNum, alphabetSet, seqLength)
-            elif(aptamerType == 'RNA'):
-                alphabetSet = 'ACGU'
-                aptamerSeqs, initialSeqNum = Apt.optimumAptamerGenerator(aptamerNum, alphabetSet, seqLength)
-            else:
-                print("Error: Simulation of %.s aptamers not supported" %aptamerType)
-                break
-            print("optimum sequences have been chosen")
-            print("SELEX Round 1 has started")
-            print("total number of sequences in initial library = "+str(initialSeqNum))
-            slctdSeqs = S.stochasticBasePairSelection_initial(alphabetSet, seqLength, aptamerSeqs, selectionThreshold, initialSeqNum, samplingSize, outputFileNames, r)
-            print("selection carried out for R0")
-            amplfdSeqs = Amplify.randomPCR_with_ErrorsAndBias(slctdSeqs, seqLength, pcrCycleNum, pcrYield, pcrErrorRate, aptamerSeqs, alphabetSet, distanceMeasure)
-            print("amplification carried out for R0")
-            outFile = outputFileNames + "_R" + str(r+1)
-            nxtRnd = open(outFile, 'w')
-            print("writing R1 seqs to file")
-            for seqIdx in amplfdSeqs:
-                seq = Apt.pseudoAptamerGenerator(seqIdx, alphabetSet, seqLength)
-                nxtRnd.write(str(seq)+'\t'+str(int(amplfdSeqs[seqIdx][0]))+'\t'+str(int(amplfdSeqs[seqIdx][1]))+'\t'+'\n') #write seqIdx, count, distance, and bias...for now
-            nxtRnd.close()
+for r in range(roundNum):
+    if(r==0):
+        if(aptamerType == 'DNA'):
+            alphabetSet = 'ACGT'
+            aptamerSeqs, initialSeqNum = Apt.optimumAptamerGenerator(aptamerNum, alphabetSet, seqLength)
+        elif(aptamerType == 'RNA'):
+            alphabetSet = 'ACGU'
+            aptamerSeqs, initialSeqNum = Apt.optimumAptamerGenerator(aptamerNum, alphabetSet, seqLength)
         else:
-            del(slctdSeqs)
-            print("SELEX Round "+str(r+1)+" has started")
-            totalSeqNum, uniqSeqNum = utils.seqNumberCounter(amplfdSeqs)
-            print("total number of sequences in initial pool = "+str(totalSeqNum))
-            print("total number of unique sequences in initial pool = "+str(int(uniqSeqNum)))
-            slctdSeqs = S.stochasticBasePairSelection(alphabetSet, seqLength, amplfdSeqs, selectionThreshold, uniqSeqNum, totalSeqNum, samplingSize, outputFileNames, r)
-            print("Selection carried for R"+str(r+1))
-            del(amplfdSeqs)
-            amplfdSeqs = Amplify.randomPCR_with_ErrorsAndBias(slctdSeqs, seqLength, pcrCycleNum, pcrYield, pcrErrorRate, aptamerSeqs, alphabetSet, distanceMeasure)
-            print("Amplification carried for R"+str(r+1))
-            outFile = outputFileNames + "_R" + str(r+1)
-            nxtRnd = open(outFile, 'w')
-            print("writing R"+str(r+1)+" seqs to file")
-            for seqIdx in amplfdSeqs:
-                seq = Apt.pseudoAptamerGenerator(seqIdx, alphabetSet, seqLength)
-                nxtRnd.write(str(seq)+'\t'+str(int(amplfdSeqs[seqIdx][0]))+'\t'+str(int(amplfdSeqs[seqIdx][1]))+'\n') #write idx and count for now
-            nxtRnd.close()
-    print("SELEX completed")
-    if(post_process==True):
-        print("Data post-processing has started...")
-        dataAnalysis(seqLength, roundNum, outputFileNames, post_process, distanceMeasure)
-        print("Data post-processing is complete")
-        print("The Simulation has ended")
+            print("Error: Simulation of %.s aptamers not supported" %aptamerType)
+            break
+        print("optimum sequences have been chosen")
+        print("SELEX Round 1 has started")
+        print("total number of sequences in initial library = "+str(initialSeqNum))
+        if(distanceMeasure == "hamming"):
+            slctdSeqs = S.stochasticBasePairSelection_initial(alphabetSet, seqLength, aptamerSeqs, selectionThreshold, initialSeqNum, samplingSize, outputFileNames, r)
+        else:
+            slctdSeqs = S.stochasticHammingSelection_initial(alphabetSet, seqLength, aptamerSeqs, selectionThreshold, initialSeqNum, samplingSize, outputFileNames, r)
+        print("selection carried out for R1")
+        amplfdSeqs = Amplify.randomPCR_with_ErrorsAndBias(slctdSeqs, seqLength, pcrCycleNum, pcrYield, pcrErrorRate, aptamerSeqs, alphabetSet, distanceMeasure)
+        print("amplification carried out for R1")
+        outFile = outputFileNames + "_R" + str(r+1)
+        nxtRnd = open(outFile, 'w')
+        print("writing R1 seqs to file")
+        for seqIdx in amplfdSeqs:
+            seq = Apt.pseudoAptamerGenerator(seqIdx, alphabetSet, seqLength)
+            nxtRnd.write(str(seq)+'\t'+str(int(amplfdSeqs[seqIdx][0]))+'\t'+str(int(amplfdSeqs[seqIdx][1]))+'\t'+'\n') #write seqIdx, count, distance, and bias...for now
+        nxtRnd.close()
     else:
-        print("The Simulation has ended without post-processing")
+        del(slctdSeqs)
+        print("SELEX Round "+str(r+1)+" has started")
+        totalSeqNum, uniqSeqNum = utils.seqNumberCounter(amplfdSeqs)
+        print("total number of sequences in initial pool = "+str(totalSeqNum))
+        print("total number of unique sequences in initial pool = "+str(int(uniqSeqNum)))
+        if(distanceMeasure == "hamming"):
+            slctdSeqs = S.stochasticBasePairSelection(alphabetSet, seqLength, amplfdSeqs, selectionThreshold, uniqSeqNum, totalSeqNum, samplingSize, outputFileNames, r)
+        else:
+            slctdSeqs = S.stochasticHammingSelection(alphabetSet, seqLength, amplfdSeqs, selectionThreshold, uniqSeqNum, totalSeqNum, samplingSize, outputFileNames, r)
+        print("Selection carried for R"+str(r+1))
+        del(amplfdSeqs)
+        amplfdSeqs = Amplify.randomPCR_with_ErrorsAndBias(slctdSeqs, seqLength, pcrCycleNum, pcrYield, pcrErrorRate, aptamerSeqs, alphabetSet, distanceMeasure)
+        print("Amplification carried for R"+str(r+1))
+        outFile = outputFileNames + "_R" + str(r+1)
+        nxtRnd = open(outFile, 'w')
+        print("writing R"+str(r+1)+" seqs to file")
+        for seqIdx in amplfdSeqs:
+            seq = Apt.pseudoAptamerGenerator(seqIdx, alphabetSet, seqLength)
+            nxtRnd.write(str(seq)+'\t'+str(int(amplfdSeqs[seqIdx][0]))+'\t'+str(int(amplfdSeqs[seqIdx][1]))+'\n') #write idx and count for now
+        nxtRnd.close()
+print("SELEX completed")
+
+if(post_process==True):
+    print("Data post-processing has started...")
+    dataAnalysis(seqLength, roundNum, outputFileNames, post_process, distanceMeasure)
+    print("Data post-processing is complete")
+    print("The Simulation has ended")
 else:
-    print("Invalid argument for distance measure")
+    print("The Simulation has ended without post-processing")
 
