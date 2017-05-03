d52db1d60dab153bf434ff9e4404b8dbe5e3ee65
tc427 <tc427@cam.ac.uk>
2017-05-03 17:58:29 +0100

    Use the correct indices to output sequences

--- Selection.py
+++ Selection.py
@@ -106,7 +106,7 @@ class Selection:
         sampleFileName = outputFileNames+"_samples_R{:03d}".format(rnd+1)
         with open(sampleFileName, 'w') as s:
             for seqIdx in randSamples:
-                seq = Apt.pseudoAptamerGenerator(seqIdx, alphabetSet, seqLength)
+                seq = Apt.pseudoAptamerGenerator(x[seqIdx][0], alphabetSet, seqLength)
                 s.write(str(seq)+'\t'+str(int(x[seqIdx][1]))+'\n')
         print("Sampling has completed")
         for i, seqIdx in enumerate(x):
@@ -195,7 +195,7 @@ class Selection:
         sampleFileName = outputFileNames+"_samples_R{:03d}".format(rnd+1)
         with open(sampleFileName, 'w') as s:
             for seqIdx in randSamples:
-                seq = Apt.pseudoAptamerGenerator(seqIdx, alphabetSet, seqLength)
+                seq = Apt.pseudoAptamerGenerator(x[seqIdx][0], alphabetSet, seqLength)
                 s.write(str(seq)+'\t'+str(int(x[seqIdx][1]))+'\n')
         print("Sampling has completed")
         for i, seqIdx in enumerate(x):
@@ -288,7 +288,7 @@ class Selection:
         sampleFileName = outputFileNames+"_samples_R{:03d}".format(rnd+1)
         with open(sampleFileName, 'w') as s:
             for seqIdx in randSamples:
-                seq = Apt.pseudoAptamerGenerator(seqIdx, alphabetSet, seqLength)
+                seq = Apt.pseudoAptamerGenerator(x[seqIdx][0], alphabetSet, seqLength)
                 s.write(str(seq)+'\t'+str(int(x[seqIdx][1]))+'\n')
         print("Sampling has completed")
         # reset all seq counts prior to selection
